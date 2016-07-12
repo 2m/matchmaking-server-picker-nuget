3ce92d597641883c32aa55ae3f70e07be9ce0a93
Martynas Mickevičius <mmartynas@gmail.com>
2016-07-12 18:47:44 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/owubvq2jye1hevpr/branch/master?svg=true)](https://ci.appveyor.com/project/2m/matchmaking-server-picker-nuget/branch/master)
+
+This is [Chocolatey](https://chocolatey.org/) package for [Matchmaking Server Picker](http://gamebanana.com/tools/5684).
+
+Install by [installing Chocolatey](https://chocolatey.org/install) and then typing:
+
+    choco install matchmaking-server-picker
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName  = 'matchmaking-server-picker' # arbitrary name for the package, used in messages
+$url          = 'http://files.gamebanana.com/tools/matchmaking_server_picker_45.zip' # download url
+$checksum     = '9d8ca18ca35930967bb3d619f4e3132a197f452a' # calcualted with 'CertUtil -hashfile <file> sha1'
+$checksumType = 'sha1'
+
+Install-ChocolateyZipPackage $packageName `
+  -Url $url `
+  -Checksum $checksum `
+  -ChecksumType $checksumType `
+  -UnzipLocation "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
